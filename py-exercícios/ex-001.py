b41900d85b1eb8b24289644023d2c5c1cdf9095a
iury-campos <isca2110@gmail.com>
2021-12-06 16:42:27 -0300

    atualizando os exercícios

---
+++ py-exercícios/ex-001.py
@@ -0,0 +1,3 @@
+#reza a  lenda que se não fizer o hello word, nós não aprendemos a linguagem
+msg = 'hello word!'
+print(msg)
---
+++ py-exercícios/ex-003.py
@@ -0,0 +1,4 @@
+valor1 = int(input('Digite o primeiro numero'))
+valor2 = int(input('Digite o segundo número'))
+soma = valor1 + valor2
+print('A soma entre {} e {} é igual a {}'.format(valor1, valor2, soma))
